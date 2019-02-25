1798e11cb184f4bdeca46eb7f370496108b9c7df
luckylyk <lionel.brouyere@gmail.com>
2019-02-25 23:02:55 +0100

    WIP tableview

--- ncachemanager/ui/table.py
+++ ncachemanager/ui/table.py
@@ -9,6 +9,9 @@ from ncachemanager.versioning import list_available_cacheversions
 from ncachemanager.cache import DYNAMIC_NODES
 
 
+RANGE_CACHED_COLOR = "#44aa22"
+RANGE_NOT_CACHED_COLOR = "#333333"
+CURRENT_TIME_COLOR = "#CC5533"
 FULL_UPDATE_REQUIRED_EVENTS = (
     om.MSceneMessage.kAfterNew,
     om.MSceneMessage.kAfterImport,
@@ -23,15 +26,18 @@ class DynamicNodesTableWidget(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(DynamicNodesTableWidget, self).__init__(parent, QtCore.Qt.Window)
         self._callbacks = []
+        self._jobs = []
         self.script_jobs = []
         self.versions = []
         self.table_model = DynamicNodeTableModel()
         self.table_view = DynamicNodeTableView()
         self.table_view.set_model(self.table_model)
         self.table_color_square = ColorSquareDelegate(self.table_view)
-        self.table_switcher = OnOffLabel(self.table_view)
+        self.table_switcher = SwitcherDelegate(self.table_view)
+        self.table_cached_range = CachedRangeDelegate(self.table_view)
         self.table_view.set_color_delegate(self.table_color_square)
         self.table_view.set_switcher_delegate(self.table_switcher)
+        self.table_view.set_cacherange_delegate(self.table_cached_range)
         self.table_model.set_nodes(list_dynamic_nodes())
 
         self.layout = QtWidgets.QVBoxLayout(self)
@@ -49,28 +55,41 @@ class DynamicNodesTableWidget(QtWidgets.QWidget):
         return [self._model.data(i, QtCore.Qt.UserRole) for i in indexes]
 
     def set_workspace(self, workspace):
-        cache_versions = list_available_cacheversions(workspace)
-        self.table_model.set_cacheversions(cache_versions)
+        cacheversions = list_available_cacheversions(workspace)
+        self.table_model.set_cacheversions(cacheversions)
 
     def register_callbacks(self):
-        for nodetype in OM_DYNAMIC_NODES:
+        for nodetype in DYNAMIC_NODES:
             self._callbacks.append(
                 om.MDGMessage.addNodeRemovedCallback(
                     self._remove_node_callback, nodetype))
             self._callbacks.append(
                 om.MDGMessage.addNodeAddedCallback(
                     self._created_node_callback, nodetype))
-            self._callbacks.append(
-                om.MNodeMessage.addNameChangedCallback(
-                    self._renamed_callback, nodetype))
+        self._callbacks.append(
+            om.MNodeMessage.addNameChangedCallback(
+                om.MObject(), self._layout_changed_callback))
         for event in FULL_UPDATE_REQUIRED_EVENTS:
             om.MSceneMessage.addCallback(event, self._full_update_callback)
+        self._jobs.append(
+            cmds.scriptJob(
+                event=[
+                    'playbackRangeChanged',
+                    self._layout_changed_callback]))
+        self._jobs.append(
+            cmds.scriptJob(
+                event=[
+                    'timeChanged',
+                    self._layout_changed_callback]))
 
     def unregister_callbacks(self):
         for callback in self._callbacks:
             om.MMessage.removeCallback(callback)
+        self._callbacks = []
+        for job in self._jobs:
+            cmds.scriptJob(kill=job, force=True)
 
-    def _remove_node_callback(self, mobject, _):
+    def _remove_node_callback(self, mobject, *unused_callbacks_args):
         if mobject.apiType() not in OM_DYNAMIC_NODES:
             return
         node = om.MFnDagNode(mobject).name()
@@ -80,16 +99,16 @@ class DynamicNodesTableWidget(QtWidgets.QWidget):
         for dynamic_node in dynamic_nodes:
             self.table_model.remove_node(dynamic_node)
 
-    def _created_node_callback(self, mobject, _):
+    def _created_node_callback(self, mobject, *unused_callbacks_args):
         if mobject.apiType() not in OM_DYNAMIC_NODES:
             return
         dynamic_node = create_dynamic_node(om.MFnDagNode(mobject).name())
         self.table_model.insert_node(dynamic_node)
 
-    def _full_update_callback(self, _):
+    def _full_update_callback(self, *unused_callbacks_args):
         self.table_model.set_nodes(list_dynamic_nodes())
 
-    def _renamed_callback(self, _, __):
+    def _layout_changed_callback(self, *unused_callbacks_args):
         self.table_model.layoutChanged.emit()
 
     def show(self):
@@ -101,46 +120,8 @@ class DynamicNodesTableWidget(QtWidgets.QWidget):
         return super(DynamicNodesTableWidget, self).closeEvent(event)
 
 
-class OnOffLabel(QtWidgets.QStyledItemDelegate):
-    """ on/off switch icon for delegate
-    / | \
-    \___/
-    """
-    ICONSIZE = 24, 24
-
-    def __init__(self, table):
-        super(OnOffLabel, self).__init__(table)
-        self._model = table.model()
-        self.icons = None
-
-    def createEditor(self, _, __, index):
-        dynamic_node = self._model.data(index, QtCore.Qt.UserRole)
-        dynamic_node.switch()
-        return
-
-    def paint(self, painter, option, index):
-        dynamic_node = self._model.data(index, QtCore.Qt.UserRole)
-        if self.icons is None:
-            self.icons = (
-                get_icon(dynamic_node.ICONS['off']),
-                get_icon(dynamic_node.ICONS['on']))
-
-        icon = self.icons[bool(dynamic_node.enable)]
-        pixmap = icon.pixmap(24, 24).scaled(
-            QtCore.QSize(*self.ICONSIZE),
-            transformMode=QtCore.Qt.SmoothTransformation)
-        rect = QtCore.QRect(
-            option.rect.center().x() - 8,
-            option.rect.center().y() - 8,
-            16, 16)
-        painter.drawPixmap(rect, pixmap)
-
-    def sizeHint(self, *args):
-        return QtCore.QSize(24, 24)
-
-
 class DynamicNodeTableModel(QtCore.QAbstractTableModel):
-    HEADERS = "", "", "Node", "Cache"
+    HEADERS = "", "", "Node", "Cache", "Range Cached", ""
 
     def __init__(self, parent=None):
         super(DynamicNodeTableModel, self).__init__(parent)
@@ -255,6 +236,10 @@ class DynamicNodeTableView(QtWidgets.QTableView):
         self.switcher_delegate = item_delegate
         self.setItemDelegateForColumn(1, item_delegate)
 
+    def set_cacherange_delegate(self, item_delegate):
+        self.cacherange_delegate = item_delegate
+        self.setItemDelegateForColumn(4, item_delegate)
+
 
 class ColorSquareDelegate(QtWidgets.QStyledItemDelegate):
 
@@ -289,3 +274,104 @@ class ColorSquareDelegate(QtWidgets.QStyledItemDelegate):
 
     def sizeHint(self, _, __):
         return QtCore.QSize(22, 22)
+
+
+class SwitcherDelegate(QtWidgets.QStyledItemDelegate):
+    ICONSIZE = 24, 24
+
+    def __init__(self, table):
+        super(SwitcherDelegate, self).__init__(table)
+        self._model = table.model()
+        self.icons = None
+
+    def createEditor(self, _, __, index):
+        dynamic_node = self._model.data(index, QtCore.Qt.UserRole)
+        dynamic_node.switch()
+        return
+
+    def paint(self, painter, option, index):
+        dynamic_node = self._model.data(index, QtCore.Qt.UserRole)
+        if self.icons is None:
+            self.icons = (
+                get_icon(dynamic_node.ICONS['off']),
+                get_icon(dynamic_node.ICONS['on']))
+
+        icon = self.icons[bool(dynamic_node.enable)]
+        pixmap = icon.pixmap(24, 24).scaled(
+            QtCore.QSize(*self.ICONSIZE),
+            transformMode=QtCore.Qt.SmoothTransformation)
+        rect = QtCore.QRect(
+            option.rect.center().x() - 8,
+            option.rect.center().y() - 8,
+            16, 16)
+        painter.drawPixmap(rect, pixmap)
+
+    def sizeHint(self, *args):
+        return QtCore.QSize(24, 24)
+
+
+class CachedRangeDelegate(QtWidgets.QStyledItemDelegate):
+    def __init__(self, table):
+        super(CachedRangeDelegate, self).__init__(table)
+        self._model = table.model()
+
+    def paint(self, painter, option, index):
+        dynamic_node = self._model.data(index, QtCore.Qt.UserRole)
+        node = dynamic_node.name
+        cacheversions = self._model.cacheversions
+        cacheversions = filter_connected_cacheversions(node, cacheversions)
+        if not cacheversions:
+            return
+        cachedstart, cachedend = cacheversions[0].infos["nodes"][node]["range"]
+        scenestart = cmds.playbackOptions(query=True, minTime=True)
+        sceneend = cmds.playbackOptions(query=True, maxTime=True)
+        invalue = percent(cachedstart, scenestart, sceneend)
+        outvalue = percent(cachedend, scenestart, sceneend)
+        bg_rect = QtCore.QRect(
+            option.rect.left() + 8,
+            option.rect.top() + 10,
+            option.rect.width() - 16,
+            option.rect.height() - 20)
+        pen = QtGui.QPen(QtGui.QColor(0, 0, 0, 0))
+        brush = QtGui.QBrush(QtGui.QColor(RANGE_NOT_CACHED_COLOR))
+        painter.setPen(pen)
+        painter.setBrush(brush)
+        painter.drawRect(bg_rect)
+
+        if outvalue == invalue:
+            return
+        left = from_percent(invalue, bg_rect.left(), bg_rect.right())
+        right = from_percent(outvalue, bg_rect.left(), bg_rect.right())
+        cached_rect = QtCore.QRect(left, bg_rect.top(), right, bg_rect.height())
+        cached_rect.setRight(right)
+        brush = QtGui.QBrush(QtGui.QColor(RANGE_CACHED_COLOR))
+        painter.setBrush(brush)
+        painter.drawRect(cached_rect)
+
+        time = cmds.currentTime(query=True)
+        left = percent(time, scenestart, sceneend)
+        left = from_percent(left, bg_rect.left(), bg_rect.right())
+        pen = QtGui.QPen(QtGui.QColor(CURRENT_TIME_COLOR))
+        pen.setWidth(2)
+        painter.setPen(pen)
+        painter.drawLine(left, bg_rect.top(), left, bg_rect.bottom())
+
+
+    def sizeHint(self, _, __):
+        return QtCore.QSize(60, 22)
+
+
+def percent(value, rangein=0, rangeout=100):
+    if value < rangein:
+        return 0
+    elif value > rangeout:
+        return 100
+    value = value - rangein
+    rangeout = rangeout - rangein
+    return (value / rangeout) * 100
+
+
+def from_percent(value, rangein=0, rangeout=100):
+    rangeout -= rangein
+    value = (value / 100) * rangeout
+    return value + rangein
