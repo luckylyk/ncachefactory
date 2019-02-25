b3ee8f1bc716d69d319a0db5955ae27b8716371d
PyLio <lionel.brouyere@gmail.com>
2019-02-25 09:27:53 +0100

    WIP

--- ncachemanager/ui/infos.py
+++ ncachemanager/ui/infos.py
@@ -7,14 +7,17 @@ class CacheInfosDialog(QtWidgets.QWidget):
         self.name = QtWidgets.QLineEdit()
         self.comment = QtWidgets.QTextEdit()
         self.nodelist = QtWidgets.QListWidgets()
+        self.close_btn = QtWidgets.QPushButton('close')
 
         self.form_layout = QtWidgets.QFormLayout()
+        self.form_layout.setMarginsContents(0, 0, 0, 0)
         self.form_layout.addRow("name", self.name)
         self.form_layout.addRow("comment", self.comment)
         self.form_layout.addRow("nodes", self.nodelist)
 
         self.layout = QtWidgets.QHBoxLayout(self)
         self.layout.addLayout(self.form_layout)
+        self.layout.addButton(self.close_btn)
 
     def set_version(self, version):
         self.name.setText(version.name)
